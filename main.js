8d48a825afcb25be3de6eb6cc125410a682dc9ea
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-08 23:16:36 +0200

    Selfwork Operatori 1

---
+++ main.js
@@ -0,0 +1,12 @@
+let numero1 = 1;
+let numero2 = 20;
+let numero3 = 50;
+let numero4 = 75;
+let numero5 = 100;
+
+let somma = numero1 + numero2 + numero3 + numero4 + numero5;
+let media = somma / 5;
+
+console.log("La somma di questi numeri equivale a " + somma);
+
+console.log("e la media equivale a " + media);
